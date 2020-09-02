ad3f5d5429a71603a3fb665e7f35f57b241a36c9
Rangel <rangersouza35@gmail.com>
2020-09-02 16:58:13 -0300

    add

--- Exe1_rev.java
+++ Exe1_rev.java
@@ -1,50 +1,83 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-class Pessoa{
+class Pessoa {
     private String nome;
-    private String data_nascimento;
+    private int ano_nascimento;
     private float altura;
 
-    public Pessoa(){
+    public Pessoa() {
         this.nome = "";
-        this.data_nascimento = "";
+        this.ano_nascimento = 0;
         this.altura = 0;
     }
-    
-    public Pessoa(String nome, String data_nascimento, float altura){
+
+    public Pessoa(String nome, int ano_nascimento, float altura) {
         this.nome = nome;
-        this.data_nascimento = data_nascimento;
+        this.ano_nascimento = ano_nascimento;
         this.altura = altura;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
 
-    public void setData_Nascimento(String data_nascimento){
-        this.data_nascimento = data_nascimento;
+    public void setAno_nascimento(int ano_nascimento) {
+        this.ano_nascimento = ano_nascimento;
     }
 
-    public String getData_Nascimento(){
-        return this.data_nascimento;
+    public int getAno_nascimento() {
+        return this.ano_nascimento;
     }
 
-    public void setAltura(float altura){
-        this.altura = altura;    
+    public void setAltura(float altura) {
+        this.altura = altura;
     }
 
-    public float getAltura(){
+    public float getAltura() {
         return this.altura;
+    }
+
+    public int getIdade(){
+        int idade;
+
+        int ano = GregorianCalendar.getInstance().get(Calendar.YEAR);
+
+        idade = ano - this.ano_nascimento;
+
+        return idade;
+    }
+
+    public void getDados() {
+        System.out.println(" Nome: " + this.nome);
+        System.out.println(" Data de nascimento: " + this.ano_nascimento);
+        System.out.println(" Altura: " + this.altura);
+
     }
 }
 
+public class Exe1_rev {
+    public int menu(){
+        int entrada = 0;
+
+        do{
+            System.out.println(" Adcionar pessoa");
 
 
-public class Exe1_rev{
+        }while();
+
+        return entrada;
+
+    }
+
     public static void main(String[] args) {
+        String nome = "";
+        int ano_nascimento = 0;
         
+        Pessoa novaPessoa = new Pessoa(nome, ano_nascimento, altura);
     }
 }
